2a8f6e29b4ab0af2aed3417fd8fe2f84f2880347
Marina DA COSTA MARTINS <marina.da-costa-martins@epitech.eu>
2023-04-24 10:24:45 +0200

    exerice liens 10,12

--- Jour_5_:_19_avril/Exercices_array/Liens_11/ex_01.js
+++ Jour_5_:_19_avril/Exercices_array/Liens_11/ex_01.js
@@ -4,3 +4,7 @@
 // En utilisant un tableau d’aide.
 // Sans utiliser un tableau d’aide.
 // Afficher le tableau résultant.
+
+const array = prompt("8", "2", "91", "23");
+const size = array.length;
+array = array.reverse();
--- Jour_5_:_19_avril/Exercices_array/Liens_12/ex_01.js
+++ Jour_5_:_19_avril/Exercices_array/Liens_12/ex_01.js
@@ -1,1 +1,15 @@
 // Ecrire un programme C qui lit la taille N d'un tableau T du type entier, remplit le tableau par des valeurs entrées au clavier et calcule et affiche ensuite la somme, le produit et la moyenne des éléments du tableau.
+const array = prompt("4","5", "2", "10");
+const size = array.length;
+
+//somme
+const somme = 0;
+for (let i = 0; i < array/lenght; i++){
+    array[i] = Number(tableau[i]);
+    somme + tableau[i];
+}
+
+//le produit ??
+
+//la moyenne
+const moyenne = 
